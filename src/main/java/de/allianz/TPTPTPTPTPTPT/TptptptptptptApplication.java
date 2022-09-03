30a75322f1c5785619ea1329c340d6b66c4e33ed
kkanto <108447856+kkanto@users.noreply.github.com>
2022-09-03 15:09:21 +0200

    Create maven project

---
+++ src/main/java/de/allianz/TPTPTPTPTPTPT/TptptptptptptApplication.java
@@ -0,0 +1,13 @@
+package de.allianz.TPTPTPTPTPTPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TptptptptptptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TptptptptptptApplication.class, args);
+	}
+
+}
